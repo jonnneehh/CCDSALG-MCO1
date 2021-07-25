2c89a499783ed0827cfdac0aab7e419771918e23
miniwhyte <ysabel_manlapaz@dlsu.edu.ph>
2021-07-25 21:04:49 +0800

    changed bubble algo

--- bubble.c
+++ bubble.c
@@ -1,16 +1,30 @@
 
-void bubble_sort(long list[], long n)
+#include <stdio.h>
+ 
+void swap(int *xp, int *yp)
 {
-  long c, d, t;
-
-  for (c = 0 ; c < n - 1; c++) {
-    for (d = 0 ; d < n - c - 1; d++) {
-      if (list[d] > list[d+1]) {
-        /* Swapping */
-        t         = list[d];
-        list[d]   = list[d+1];
-        list[d+1] = t;
-      }
-    }
-  }
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)     
+ 
+       // Last i elements are already in place  
+       for (j = 0; j < n-i-1; j++)
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
