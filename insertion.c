001eea8fed7e3be7cc437f34f5b288fb4c772ce8
MraccraM <sibalmarcl@gmail.com>
2021-07-26 19:45:41 +0800

    added 'count' to insertion and selection sort, double check for correctness

--- insertion.c
+++ insertion.c
@@ -3,22 +3,34 @@
 #include <stdio.h>
  
 /* Function to sort an array using insertion sort*/
-void insertionSort(int arr[], int n)
+int insertionSort(int arr[], int n)
 {
-    int i, key, j;
+    int i, key, j, count;
+    
+    count = 1;
+    
+    count++; // enter loop
     for (i = 1; i < n; i++) {
+    	count++; // adds to counter per run of loop
+    	
         key = arr[i];
         j = i - 1;
  
         /* Move elements of arr[0..i-1], that are
           greater than key, to one position ahead
           of their current position */
+        count++; // enter while loop
         while (j >= 0 && arr[j] > key) {
+        	count++; // adds to count per run of loop
             arr[j + 1] = arr[j];
             j = j - 1;
         }
+        
         arr[j + 1] = key;
+        count++; // adds to count when new key is assigned
     }
+    
+    return count; // returns final value for count
 }
  
 // A utility function to print an array of size n
--- selection.c
+++ selection.c
@@ -1,29 +1,48 @@
 // C program for implementation of selection sort
 #include <stdio.h>
  
-void swap(int *xp, int *yp)
+void swap(int *xp, int *yp int *count)
 {
     int temp = *xp;
+    *count++; // add count when line above is executed
+    
     *xp = *yp;
+    *count++; // add count when line above is executed
+    
     *yp = temp;
+    *count++; // add count when line above is executed
 }
  
-void selectionSort(int arr[], int n)
+int selectionSort(int arr[], int n)
 {
-    int i, j, min_idx;
+    int i, j, min_idx, count;
+    
+    count = 1;
  
     // One by one move boundary of unsorted subarray
+    count++; // enter loop
     for (i = 0; i < n-1; i++)
     {
+    	count++; // count per loop run
         // Find the minimum element in unsorted array
         min_idx = i;
-        for (j = i+1; j < n; j++)
-          if (arr[j] < arr[min_idx])
-            min_idx = j;
- 
+        
+        count++; // add count when loop is entered
+        for (j = i+1; j < n; j++){
+        	count++; // count per loop run
+        
+        	count++;// add count when if statement is checked
+        	if (arr[j] < arr[min_idx]){
+        		min_idx = j;
+				count++; // count per successful if
+			}
+		}
         // Swap the found minimum element with the first element
-        swap(&arr[min_idx], &arr[i]);
+        swap(&arr[min_idx], &arr[i],&count);
+        count++; // add count when swap is called
     }
+    
+    return count; // returns final value for count
 }
  
 /* Function to print an array */
