01153949e6235a53ddf9b70619d6d0a0cf834131
miniwhyte <ysabel_manlapaz@dlsu.edu.ph>
2021-07-26 17:57:40 +0800

    added counter in bubble sort

--- bubble.c
+++ bubble.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
  
-void swap(int *xp, int *yp)
-{
+void swap(int *xp, int *yp, int *count)
+{   
     int temp = *xp;
+    *count++;
+
     *xp = *yp;
+    *count++;
+
     *yp = temp;
+    *count++;
 }
  
 // A function to implement bubble sort
-void bubbleSort(int arr[], int n)
+int bubbleSort(int arr[], int n)
 {
-   int i, j;
-   for (i = 0; i < n-1; i++)     
- 
-       // Last i elements are already in place  
+   int i, j, count;
+
+   count = 1;
+
+   count++; //first count of going inside for loop
+   for (i = 0; i < n-1; i++) 
+   { 
+       count++; //count when inside for loop  
+       
+        // Last i elements are already in place
+
+       count++; //first count of going inside for loop
        for (j = 0; j < n-i-1; j++)
+       {   
+           count++;  //count when inside for loop  
+
+           count++;  //first count of going inside if statement
            if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
+           {   
+               count++; //first count of going inside swap function
+               swap(&arr[j], &arr[j+1], &count);
+           }
+              
+       }
+
+   }     
+    
+    return count;
+             
 }
  
 /* Function to print an array */
