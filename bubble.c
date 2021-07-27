cc181bb22209822782dae3c74c43afa6841f335a
miniwhyte <ysabel_manlapaz@dlsu.edu.ph>
2021-07-27 21:25:43 +0800

    removed print array in bubble

--- bubble.c
+++ bubble.c
@@ -46,11 +46,3 @@ int bubbleSort(int arr[], int n)
              
 }
  
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
