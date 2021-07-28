864ca5ce584aa1b297c36e12ef82c05c18460e9e
jonnneehh <you@example.com>
2021-07-28 23:52:30 +0800

    Better Interface. Main runs 5 times now.

--- TheRealMain.c
+++ TheRealMain.c
@@ -8,36 +8,37 @@ long int count = 0;
 string30 sCount;
 
 int main (){
+	int i;
+	printf("MAX_N: %d\n\ni   %-15s %-25s\n", MAX_N, "Count", "Time in milliseconds");
 	
-	struct timespec begin, end; 
-	
-	#if CPU_TIME
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
-	
-	#else
-	clock_gettime(CLOCK_REALTIME, &begin);
-	
-	#endif 
-	printf("Generating Data...\n");
-	generateData(data, MAX_N);
-	
-	printf("Sorting Data...\n");
-	count = heapSort(data, MAX_N);
-	
-	#if CPU_TIME 
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); 
-	
-	#else
-	clock_gettime(CLOCK_REALTIME, &end);
-	
-	#endif
-	
-	long seconds = end.tv_sec - begin.tv_sec;
-    long nanoseconds = end.tv_nsec - begin.tv_nsec;
-    double elapsed = seconds + nanoseconds*1e-9;
-    
-    convertToCommaFormat(count, sCount);
-	printf("\nMAX_N: %d\nCount = %s\n", MAX_N, sCount);
-	printf("CPU Time measured: %lf in milliseconds.\n", elapsed * 1000);
+	for(i = 0; i < 5; i++){
+		struct timespec begin, end; 
+		
+		#if CPU_TIME
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+		
+		#else
+		clock_gettime(CLOCK_REALTIME, &begin);
+		
+		#endif 
+		generateData(data, MAX_N);
+		count = heapSort(data, MAX_N);
+		
+		#if CPU_TIME 
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); 
+		
+		#else
+		clock_gettime(CLOCK_REALTIME, &end);
+		
+		#endif
+		
+		long seconds = end.tv_sec - begin.tv_sec;
+		long nanoseconds = end.tv_nsec - begin.tv_nsec;
+		double elapsed = seconds + nanoseconds*1e-9;
+		
+		convertToCommaFormat(count, sCount);
+		printf("%-3d %-15s", i, sCount);
+		printf(" %-25lf\n", elapsed * 1000);	
+	}
 	return 0;
 }
